46074a944b937d52df86f8423c9700d4540915cb
kangpp <353327533@qq.com>
2017-06-12 15:15:09 +0800

    扫描保存的图片是黑色的？？？未解决

--- 自定义相机——人脸识别/ViewController.swift
+++ 自定义相机——人脸识别/ViewController.swift
@@ -25,7 +25,7 @@ class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate, 
     lazy var faceBoxView: UIImageView = {
         let imgView = UIImageView()
         imgView.image = UIImage(named: "face")
-//        self.view.addSubview(imgView)
+        // self.view.addSubview(imgView)
         self.preview?.addSublayer(imgView.layer)
         return imgView
     }()
@@ -49,8 +49,10 @@ class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate, 
         return capImg
     }()
     
+    var stillImageOutput: AVCaptureStillImageOutput = AVCaptureStillImageOutput()
     
     func setupCamera() {
+        
         //session
         session = AVCaptureSession()
         session?.sessionPreset = AVCaptureSessionPresetPhoto
@@ -62,8 +64,15 @@ class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate, 
         // 设置为前置摄像头
         let devices = AVCaptureDeviceDiscoverySession(deviceTypes: [AVCaptureDeviceType.builtInWideAngleCamera], mediaType: AVMediaTypeVideo, position: AVCaptureDevicePosition.front)
         for device in (devices?.devices)! {
+            // 自动白平衡
+            if device.isWhiteBalanceModeSupported(.autoWhiteBalance) {
+                device.isWhiteBalanceModeSupported(.autoWhiteBalance)
+            }
+            // 自动曝光
+            device.isWhiteBalanceModeSupported(.autoWhiteBalance)
             self.device = device
         }
+        
         
         // input
         do {
@@ -71,28 +80,31 @@ class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate, 
         } catch let error as NSError {
             print("error: \(error.localizedDescription)")
         }
-        if (session?.canAddInput(input))! {
-            session?.addInput(input)
-        }
-        
+
         // output // 人脸识别
         output = AVCaptureMetadataOutput()
         output?.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
-        if (session?.canAddOutput(output))! {
-            session?.addOutput(output)
-        }
-        output?.metadataObjectTypes = [AVMetadataObjectTypeFace]
-
+        
         // preview
         preview = AVCaptureVideoPreviewLayer(session: session)
         preview?.videoGravity = AVLayerVideoGravityResizeAspectFill
-        preview?.frame = CGRect(x: (self.view.frame.width-200)/2.0, y: 150, width: 200, height: 200)
+        preview?.frame = CGRect(x: (self.view.frame.width-200)/2.0, y: 50, width: 200, height: 200)
         preview?.cornerRadius = 100
         preview?.borderColor = UIColor.gray.cgColor
         preview?.borderWidth = 3
-//        preview?.frame = self.view.bounds
+        // preview?.frame = self.view.bounds
         self.view.layer.insertSublayer(preview!, at: 0)
-    
+        
+        session?.beginConfiguration()
+        if (session?.canAddInput(input))! {
+            session?.addInput(input)
+        }
+        if (session?.canAddOutput(output))! {
+            session?.addOutput(output)
+            output?.metadataObjectTypes = [AVMetadataObjectTypeFace]
+        }
+        session?.commitConfiguration()
+        
         session?.startRunning()
     }
 
@@ -101,158 +113,97 @@ class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate, 
 
         
         setupCamera()
+        
+
     }
     
-
-    
-    
-    
-    
-//    func captureOutput(captureOutput: AVCaptureOutput!, didOutputSampleBuffer sampleBuffer: CMSampleBuffer!, fromConnection connection: AVCaptureConnection!) {
+//    func capturePhotoHHH() {
+//        isComplete = true
+//
+//        session?.addOutput(cameraOutput)
 //        
-//        if(self.isStartGetImage)
-//        {
-//            let resultImage = sampleBufferToImage(sampleBuffer: sampleBuffer)
+//        let connection = cameraOutput.connection(withMediaType: AVMediaTypeVideo)
+//        connection?.videoOrientation = AVCaptureVideoOrientation.portrait
+//
+//        
+//        stillImageOutput.captureStillImageAsynchronously(from: connection) { (imageDataSampleBuffer, error) in
+//
+//            let imageData = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(imageDataSampleBuffer)
+//            let image = UIImage(data: imageData!)
 //            
-//            let context = CIContext(options:[kCIContextUseSoftwareRenderer:true])
-//            let detecotr = CIDetector(ofType:CIDetectorTypeFace,  context:context, options:[CIDetectorAccuracy: CIDetectorAccuracyHigh])
-//            
-//            
-//            
-//            
-//            let ciImage = CIImage(image: resultImage)
-//            
-//            let results:NSArray = detecotr!.featuresInImage(ciImage,options: ["CIDetectorImageOrientation" : 6])
-//            
-//            for r in results {
-//                let face:CIFaceFeature = r as! CIFaceFeature;
-//                let faceImage = UIImage(CGImage: context.createCGImage(ciImage!, fromRect: face.bounds),scale: 1.0, orientation: .Right)
-//                
-//                NSLog("Face found at (%f,%f) of dimensions %fx%f", face.bounds.origin.x, face.bounds.origin.y,pickUIImager.frame.origin.x, pickUIImager.frame.origin.y)
-//                
-//                DispatchQueue.main.async() {
-//                    if (self.isStartGetImage)
-//                    {
-//                        self.dismiss(animated: true, completion: nil)
-//                        self.didReceiveMemoryWarning()
-//                        
-//                        self.callBack!(face: faceImage!)
-//                    }
-//                    self.isStartGetImage = false
-//                }
-//            }
+//            self.capturedImgView.image = image
+//            self.isComplete = true
+//            self.session?.stopRunning()
 //        }
-//    }
-//    private func sampleBufferToImage(sampleBuffer: CMSampleBuffer!) -> UIImage {
-//        let imageBuffer: CVImageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer)!
-//        CVPixelBufferLockBaseAddress(imageBuffer, CVPixelBufferLockFlags(rawValue: 0))
-//        let baseAddress = CVPixelBufferGetBaseAddressOfPlane(imageBuffer, 0)
-//        
-//        let bytesPerRow = CVPixelBufferGetBytesPerRow(imageBuffer)
-//        let width = CVPixelBufferGetWidth(imageBuffer)
-//        let height = CVPixelBufferGetHeight(imageBuffer)
-//        
-//        let colorSpace: CGColorSpace = CGColorSpaceCreateDeviceRGB()
-//        
-//        let bitsPerCompornent = 8
-//        var bitmapInfo = CGBitmapInfo(rawValue: (CGBitmapInfo.byteOrder32Little.rawValue | CGImageAlphaInfo.premultipliedFirst.rawValue) as UInt32)
-//        
-//        
-//        let newContext = CGBitmapContextCreate(baseAddress, width, height, bitsPerCompornent, bytesPerRow, colorSpace, bitmapInfo.rawValue) as CGContext
-//        
-//        let imageRef: CGImage = CGBitmapContextCreateImage(newContext)
-//        let resultImage = UIImage(CGImage: imageRef, scale: 1.0, orientation: UIImageOrientation.Right)!
-//        
-//        return resultImage
 //    }
     
     func capturePhoto() {
         isComplete = true
-        let connection = cameraOutput.connection(withMediaType: AVMediaTypeVideo)
-        if (connection != nil) {
+        
+        session?.addOutput(cameraOutput)
+        
+        if let connection = cameraOutput.connection(withMediaType: AVMediaTypeVideo) {
+            connection.videoOrientation = AVCaptureVideoOrientation.portrait
             let settings = AVCapturePhotoSettings()
             let previewPixelType = settings.availablePreviewPhotoPixelFormatTypes.first!
-            let previewFormat = [kCVPixelBufferPixelFormatTypeKey as String: previewPixelType,
-                                 kCVPixelBufferWidthKey as String: 160,
-                                 kCVPixelBufferHeightKey as String: 160]
+            let previewFormat = [
+                kCVPixelBufferPixelFormatTypeKey as String: previewPixelType,
+                kCVPixelBufferWidthKey as String: 200,
+                kCVPixelBufferHeightKey as String: 200
+            ]
             settings.previewPhotoFormat = previewFormat
             cameraOutput.capturePhoto(with: settings, delegate: self)
-            session?.addOutput(cameraOutput)
         }
-
+        session?.stopRunning()
     }
+    
+    
+    
+
+    
+
+    
+    
 
     // MARK: - AVCapturePhotoCaptureDelegate
-//    func capture(_ captureOutput: AVCapturePhotoOutput, didFinishProcessingPhotoSampleBuffer photoSampleBuffer: CMSampleBuffer?, previewPhotoSampleBuffer: CMSampleBuffer?, resolvedSettings: AVCaptureResolvedPhotoSettings, bracketSettings: AVCaptureBracketedStillImageSettings?, error: Error?) {
-//        print("识别出人脸")
-//
-//        if let error = error {
-//            print(error.localizedDescription)
-//        }
-//
-//        if  let sampleBuffer = photoSampleBuffer,
-//            let previewBuffer = previewPhotoSampleBuffer,
-//            let dataImage =  AVCapturePhotoOutput.jpegPhotoDataRepresentation(forJPEGSampleBuffer:  sampleBuffer, previewPhotoSampleBuffer: previewBuffer) {
-//            print(UIImage(data: dataImage)?.size as Any)
-//            
-//            let dataProvider = CGDataProvider(data: dataImage as CFData)
-//            let cgImageRef: CGImage! = CGImage(jpegDataProviderSource: dataProvider!, decode: nil, shouldInterpolate: true, intent: .defaultIntent)
-//            let image = UIImage(cgImage: cgImageRef, scale: 1.0, orientation: UIImageOrientation.right)
-//            
-//            self.capturedImgView.image = image
-//        } else {
-//            print("some error here")
-//        }
-//        
-//    }
-    
-//    func capturePhoto() {
-//        let connection = cameraOutput.connection(withMediaType: AVMediaTypeVideo)
-//        let settings = AVCapturePhotoSettings()
-//        let previewPixelType = settings.availablePreviewPhotoPixelFormatTypes.first!
-//        let previewFormat = [kCVPixelBufferPixelFormatTypeKey as String: previewPixelType,
-//                             kCVPixelBufferWidthKey as String: 160,
-//                             kCVPixelBufferHeightKey as String: 160,
-//                             ]
-//        settings.previewPhotoFormat = previewFormat
-//        self.cameraOutput.capturePhoto(with: settings, delegate: self)
-//        
-//    }
-    
-    
-//    func capture(_ captureOutput: AVCapturePhotoOutput, didFinishProcessingPhotoSampleBuffer photoSampleBuffer: CMSampleBuffer?, previewPhotoSampleBuffer: CMSampleBuffer?, resolvedSettings: AVCaptureResolvedPhotoSettings, bracketSettings: AVCaptureBracketedStillImageSettings?, error: Error?) {
-//        print("来到这了")
-//        if let error = error {
-//            print(error.localizedDescription)
-//        }
-//        
-//        if let sampleBuffer = photoSampleBuffer, let previewBuffer = previewPhotoSampleBuffer, let dataImage = AVCapturePhotoOutput.jpegPhotoDataRepresentation(forJPEGSampleBuffer: sampleBuffer, previewPhotoSampleBuffer: previewBuffer) {
-////            print(image: UIImage(data: dataImage)?.size)
-//        } else {
-//            
-//        }
-//        
-//    }
+    func capture(_ captureOutput: AVCapturePhotoOutput, didFinishProcessingPhotoSampleBuffer photoSampleBuffer: CMSampleBuffer?, previewPhotoSampleBuffer: CMSampleBuffer?, resolvedSettings: AVCaptureResolvedPhotoSettings, bracketSettings: AVCaptureBracketedStillImageSettings?, error: Error?) {
+        print("识别出人脸")
 
+        if let error = error {
+            print(error.localizedDescription)
+        }
 
+        if  let sampleBuffer = photoSampleBuffer,
+            let previewBuffer = previewPhotoSampleBuffer,
+            let dataImage =  AVCapturePhotoOutput.jpegPhotoDataRepresentation(forJPEGSampleBuffer:  sampleBuffer, previewPhotoSampleBuffer: previewBuffer) {
+            print(UIImage(data: dataImage)?.size as Any)
+            
+            let dataProvider = CGDataProvider(data: dataImage as CFData)
+            let cgImageRef: CGImage! = CGImage(jpegDataProviderSource: dataProvider!, decode: nil, shouldInterpolate: true, intent: .defaultIntent)
+            let image = UIImage(cgImage: cgImageRef, scale: 1.0, orientation: UIImageOrientation.leftMirrored)
+            
+            self.capturedImgView.image = image
+        } else {
+            print("some error here")
+        }
+        
+    }
+    
 
     // MARK: - AVCaptureMetadataOutputObjectsDelegate
     func captureOutput(_ captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [Any]!, from connection: AVCaptureConnection!) {
-//        print("*****************")
-//        print(metadataObjects)
-//        print("*****************")
         
-//        if isComplete {
-//            return
-//        }
+        if isComplete == true {
+            return
+        }
         for item in metadataObjects {
             if (item as! AVMetadataObject).type == AVMetadataObjectTypeFace {
                 let transform: AVMetadataObject = (preview?.transformedMetadataObject(for: item as? AVMetadataObject))!
                 DispatchQueue.global().async {
                     DispatchQueue.main.async {
                         self.showFaceImage(withFrame: transform.bounds)
-                        self.capturePhoto()
                     }
+                    self.capturePhoto()
                 }
             }
         }
@@ -283,7 +234,82 @@ class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate, 
 
     }
     
+    private func sampleBufferToImage(sampleBuffer: CMSampleBuffer!) -> UIImage {
+        let imageBuffer: CVImageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer)!
+        CVPixelBufferLockBaseAddress(imageBuffer, CVPixelBufferLockFlags(rawValue: 0))
+        let baseAddress = CVPixelBufferGetBaseAddressOfPlane(imageBuffer, 0)
+        
+        let bytesPerRow = CVPixelBufferGetBytesPerRow(imageBuffer)
+        let width = CVPixelBufferGetWidth(imageBuffer)
+        let height = CVPixelBufferGetHeight(imageBuffer)
+        
+        let colorSpace: CGColorSpace = CGColorSpaceCreateDeviceRGB()
+        
+        let bitsPerCompornent = 8
+        let bitmapInfo = CGBitmapInfo(rawValue: (CGBitmapInfo.byteOrder32Little.rawValue | CGImageAlphaInfo.premultipliedFirst.rawValue) as UInt32)
+        
+        
+        let newContext = CGContext(data: baseAddress, width: width, height: height, bitsPerComponent: bitsPerCompornent, bytesPerRow: bytesPerRow, space: colorSpace, bitmapInfo: bitmapInfo.rawValue)!
+        
+        let imageRef: CGImage = newContext.makeImage()!
+        //        let resultImage = UIImage(CGImage: imageRef, scale: 1.0, orientation: UIImageOrientation.Right)!
+        
+        let image = UIImage(cgImage: imageRef, scale: 1.0, orientation: UIImageOrientation.leftMirrored)
+        
+        return image
+    }
+    func image(fromSampleBuffer sampleBuffer: CMSampleBuffer) -> UIImage {
+        let imageBuffer: CVImageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer)!
+        CVPixelBufferLockBaseAddress(imageBuffer, CVPixelBufferLockFlags(rawValue: 0))
+        let baseAddress = CVPixelBufferGetBaseAddress(imageBuffer)
+        let bytesPerRow: size_t = CVPixelBufferGetBytesPerRow(imageBuffer)
+        let width: size_t = CVPixelBufferGetWidth(imageBuffer)
+        let height: size_t = CVPixelBufferGetHeight(imageBuffer)
+        let colorSpace: CGColorSpace = CGColorSpaceCreateDeviceRGB()
+        let bitmapInfo = CGBitmapInfo(rawValue: (CGBitmapInfo.byteOrder32Little.rawValue | CGImageAlphaInfo.premultipliedFirst.rawValue) as UInt32)
+        
+        let context: CGContext = CGContext.init(data: baseAddress, width: width, height: height, bitsPerComponent: 8, bytesPerRow: bytesPerRow, space: colorSpace, bitmapInfo: bitmapInfo.rawValue)!
+        
+        let imageRef: CGImage = context.makeImage()!
+        
+        
+        CVPixelBufferUnlockBaseAddress(imageBuffer, CVPixelBufferLockFlags(rawValue: 0))
+        
+        let image: UIImage = UIImage(cgImage: imageRef)
+        
+        return image
+    }
     
+    
+    func captureOutput(_ fromcaptureOutput: AVCaptureOutput!, didOutputSampleBuffer sampleBuffer: CMSampleBuffer!, from connection: AVCaptureConnection!) {
+        print("开不了")
+    }
+
+    
+    
+    //    private func sampleBufferToImage(sampleBuffer: CMSampleBuffer!) -> UIImage {
+    //        let imageBuffer: CVImageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer)!
+    //        CVPixelBufferLockBaseAddress(imageBuffer, CVPixelBufferLockFlags(rawValue: 0))
+    //        let baseAddress = CVPixelBufferGetBaseAddressOfPlane(imageBuffer, 0)
+    //
+    //        let bytesPerRow = CVPixelBufferGetBytesPerRow(imageBuffer)
+    //        let width = CVPixelBufferGetWidth(imageBuffer)
+    //        let height = CVPixelBufferGetHeight(imageBuffer)
+    //
+    //        let colorSpace: CGColorSpace = CGColorSpaceCreateDeviceRGB()
+    //
+    //        let bitsPerCompornent = 8
+    //        let bitmapInfo = CGBitmapInfo(rawValue: (CGBitmapInfo.byteOrder32Little.rawValue | CGImageAlphaInfo.premultipliedFirst.rawValue) as UInt32)
+    //
+    //
+    //        let newContext = CGContext(data: baseAddress, width: width, height: height, bitsPerComponent: bitsPerCompornent, bytesPerRow: bytesPerRow, space: colorSpace, bitmapInfo: bitmapInfo.rawValue)!
+    //
+    //        let imageRef: CGImage = newContext.makeImage()!
+    //        
+    //        let resultImg = UIImage(cgImage: imageRef, scale: 1.0, orientation: UIImageOrientation.right)
+    //        
+    //        return resultImg
+    //    }
 
 }
 
